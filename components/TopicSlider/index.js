5fa3c2d046b137744c57aa8caec31298dbcfd82c
PrinceNick-BL <princenickballo@gmail.com>
2022-10-05 22:15:31 +0200

    wip

--- components/TopicSlider/index.js
+++ components/TopicSlider/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './index.module.scss'
+import Image from 'next/image'
 
 export default function TopicSlider() {
     return (
@@ -7,7 +8,16 @@ export default function TopicSlider() {
             <h2 className={styles.textColor}>React JS</h2>
             <div className={styles.container}>
                 <div className={styles.top}> 
-
+                    <Image
+                        src={"https://picsum.photos/500/300"}
+                        width={500}
+                        height={300}
+                        layout={"responsive"}
+                        className={styles.img}
+                    />
+                    <div className={styles.desc}>
+                        Lorem enim tempor Lorem excepteur aliquip reprehenderit. Enim id aliqua nisi officia sit. Magna veniam adipisicing cillum aliqua ut cillum do et id adipisicing esse quis ad. Anim ex qui quis sint commodo laboris laborum mollit dolore ipsum. Ullamco laboris mollit ut nisi. Do pariatur esse id aute reprehenderit ut enim sit velit pariatur sit.  
+                    </div>
                 </div>
                 <div  className={styles.bottom}>
                     <div className={styles.positions}>2 sur 4</div>
