ac2c87398726ffa98683613cfbb6e395de8f2aa9
PrinceNick-BL <princenickballo@gmail.com>
2022-10-05 21:15:40 +0200

    wip

--- components/TopicSlider/index.js
+++ components/TopicSlider/index.js
@@ -3,12 +3,15 @@ import styles from './index.module.scss'
 
 export default function TopicSlider() {
     return (
-        <div className={styles.container}>
-            <div className={styles.top}> 
+        <div className={styles.wrapper}>
+            <h2 className={styles.textColor}>Topic cluster and topics</h2>
+            <div className={styles.container}>
+                <div className={styles.top}> 
 
-            </div>
-            <div  className={styles.bottom}>
-                
+                </div>
+                <div  className={styles.bottom}>
+                    
+                </div>
             </div>
         </div>
     )
