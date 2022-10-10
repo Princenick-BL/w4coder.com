84628c9c2f33683929043042b5db803deb6ca4d7
PrinceNick-BL <princenickballo@gmail.com>
2022-10-10 20:41:46 +0200

    WIP

--- components/StickyMenu/index.js
+++ components/StickyMenu/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import styles from './index.module.scss'
+import {RiMovieFill,RiHome2Fill} from 'react-icons/ri'
 
 export default function StickyMenu() {
   return (
-    <div className={styles.stickyMenu}>StickyMenu</div>
+    <div className={styles.stickyMenu}>
+        <RiHome2Fill size={24} style={{marginRight:"5px",marginLeft:"5px"}}/>
+        <RiMovieFill size={24} style={{marginRight:"5px",marginLeft:"5px"}}/>
+    </div>
   )
 }
