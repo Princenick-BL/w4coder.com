46c4a6595191e609da178d5742e02ab74e96a31a
PrinceNick-BL <princenickballo@gmail.com>
2022-10-10 21:46:03 +0200

    WIP

--- components/Home/Mobile/MoviesPage.js
+++ components/Home/Mobile/MoviesPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
+import styles from './index.module.scss'
 import StickyMenu from '../../StickyMenu'
 
 export default function MoviesPage({setCurrentView}) {
   return (
-    <div>
-        <StickyMenu setCurrentView={setCurrentView}/>
-    </div>
+    <>
+        <Head>
+
+        </Head>
+        <StickyMenu setCurrentView={setCurrentView} />
+        <main className={styles.main} >
+        </main>
+    </>
   )
 }
--- components/Home/Mobile/StoriesPage.js
+++ components/Home/Mobile/StoriesPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Head from 'next/head'
+import styles from './index.module.scss'
 import StickyMenu from '../../StickyMenu'
 
 export default function StoriesPage({setCurrentView}) {
   return (
-    <div id="storiesTop">
-        <StickyMenu setCurrentView={setCurrentView}/>
-    </div>
+    <>
+        <Head>
+
+        </Head>
+        <div id="storiesTop">
+            <StickyMenu setCurrentView={setCurrentView}/>
+            <main className={styles.main} >
+            </main>
+        </div>
+    </>
   )
 }
