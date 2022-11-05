e7c6ccc01f1ec5bc98f2ae45f8548de25730cc0f
PrinceNick-BL <princenickballo@gmail.com>
2022-11-05 19:46:35 +0100

    update

--- components/Header/Desktop.js
+++ components/Header/Desktop.js
@@ -1,8 +1,66 @@
 import React from 'react'
 import Logo from '../Logo'
 import styles from './header.module.scss'
+import Link from 'next/link'
 
 export default function Desktop() {
+  const [searchResult,setSearchResult]=useState([])
+
+    const searchRef = useRef(null);
+    //useAmpStoryPlayer(loadPlayer(playerRef))
+    const stickyHeader = useRef()
+
+    useLayoutEffect(() => {
+        const mainHeader = document.getElementById('mainHeader')
+        let fixedTop = stickyHeader.current.offsetTop
+        const fixedHeader = () => {
+          if (window.pageYOffset > fixedTop) {
+            mainHeader.classList.add('fixedTop')
+          } else {
+            mainHeader.classList.remove('fixedTop')
+          }
+        }
+        window.addEventListener('scroll', fixedHeader)
+      }, [])
+     
+    
+    
+      const searching = async (e)=>{
+        const mainHeaderResult = document.getElementById("mainHeader")
+        if(e?.target?.value?.length > 0){
+          mainHeaderResult.classList.add('searching')
+          const res = await searchArticle(e?.target?.value)
+          console.log(res)
+          setSearchResult(res)
+        }else{
+          mainHeaderResult.classList.remove('searching')
+          setSearchResult([])
+        }
+    
+      }
+    
+      function useSearchStop(ref) {
+        useEffect(() => {
+    
+          /**
+           * Alert if clicked on outside of element
+           */
+          function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+              const mainHeaderResult = document.getElementById("mainHeader")
+              mainHeaderResult.classList.remove('searching')
+              setSearchResult([])
+            }
+          }
+          // Bind the event listener
+          document.addEventListener("mousedown", handleClickOutside);
+          return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+          };
+        }, [ref]);
+      }
+      useSearchStop(searchRef);
   return (
     <div className={styles.deskHead}>
         <Logo style={{fontSize:"1.5rem"}}/>
