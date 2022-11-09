80406dd4b74ffd81cf52206197868486f3a67a81
PrinceNick-BL <princenickballo@gmail.com>
2022-11-09 21:26:02 +0100

    WIp

---
+++ components/AmpStoryPlayer/index.js
@@ -0,0 +1,101 @@
+import React,{useState} from 'react'
+import Head from 'next/head';
+import Image from 'next/image';
+
+
+const Widget = ({img,pos,color,text,url,onclick}) =>{
+    return(
+        <div className="entry-point-card-container" 
+          onClick={(e)=>{        
+            onclick()
+            document.getElementById("homTop").style.overflowY="hidden"
+            initializeWidget(pos)
+        }}>
+          <div 
+            className='img-container' 
+            style={{ 
+              borderColor: `${color} !important`,
+              border: `2px solid ${color}`,
+  
+            }}>
+              <Image 
+                  className='img' 
+                  width={100} 
+                  height={100} 
+                  src={img}
+                  alt={text}  
+                />
+  
+            </div>
+            <div>
+            <span className="entry-point-card-title">{text}</span>
+            </div>
+        </div>
+    )
+  }
+export default function AmpStoryPlayer() {
+    const [show,setShow]=useState(false)
+
+    const [stories,setStories] = useState([
+        {
+            url: "https://wsdemos.uc.r.appspot.com/ampfest/s1",
+            img : 'https://assets.codepen.io/1780597/4.png',
+            color : '#FF6F32',
+            text : 'Q&A with ZOE Newman'
+        },
+        {
+            url: "https://wsdemos.uc.r.appspot.com/ampfest/s2",
+            img : "https://assets.codepen.io/1780597/1.png" ,
+            color :"#E6AD1C" ,
+            text : "24 Hours in New York City"
+        },
+        {
+            url: "https://wsdemos.uc.r.appspot.com/ampfest/s3",
+            img : "https://assets.codepen.io/1780597/3.png" ,
+            color : "#466FFF",
+            text : "The Next King of the Sea"
+        },
+        {
+            url: "https://wsdemos.uc.r.appspot.com/ampfest/s4",
+            img : "https://assets.codepen.io/1780597/2.png",
+            color : "#4CA47C",
+            text : "Spark a Passion for Reading"
+        }
+    ])
+
+    return (
+        <>
+            <div className="viewport">
+                <div className="entry-point-container">
+                    <div className="circular-entry-point">
+                        <div className="entry-points">
+                        {stories.map((story,index)=>{
+                            return(
+                            <Widget
+                                key={index}
+                                pos={index}
+                                img={story.img}
+                                color = {story.color}
+                                text = {story.text}
+                                url={story.url}
+                                onclick={(e)=>{setShow(!show)}}
+                            />
+                            )
+                        })}
+                        </div>
+                    </div>
+                    </div>
+                    <br></br>
+                    <div className={`lightbox ${show?"show":""}`}>
+                    <amp-story-player style={{width:"100vw",height:"100vh"}} layout="responsive" width="360" height="600"id="player2" >
+                        {stories.map((story,index)=>{
+                            return(
+                            <a key={index} href={story?.url}></a>
+                            )
+                        })}
+                    </amp-story-player>
+                </div>
+            </div>   
+        </>
+    )
+}
--- components/HomePage/Mobile.js
+++ components/HomePage/Mobile.js
@@ -3,82 +3,80 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { getArticle ,getTopArticles,searchArticle} from '../../services/articles'
 import Slide from './components/Mobile/Card'
 import styles from './homepage.module.scss'
-
+import AmpStoryPlayer from '../AmpStoryPlayer';
 export default function Mobile({page1,topA}) {
     const [pages,setPages] = useState([])
     const [pageNum,setPageNum] = useState(1)
     const [hasMore,setHasMore] = useState(true)
-
     const fetchMoreData = async ()=>{
-
-        const res =  await getArticle({filter:{
-          page : pageNum + 1
-        }})
-        setPageNum(pageNum + 1)
-        setPages([...pages,...res])
-        if(res.length === 0){
-          setHasMore(false)
-        }
+      const res =  await getArticle({filter:{
+        page : pageNum + 1
+      }})
+      setPageNum(pageNum + 1)
+      setPages([...pages,...res])
+      if(res.length === 0){
+        setHasMore(false)
+      }
     }
 
-    return (
-        <div className={styles.editorial}>
-            <div className={styles.editorialContent}>
-              <div
-                className={styles.mainSlideShow}
-              >
-                {page1 ? page1.map((article,index)=>{
-                  return(
-                    <Slide key={index} article={article} style={{height:"100%"}}/>
-                  )
-                }):(
-                  <></>
-                )}
-              </div>
-              
-              {/* <TopicSlider/> */}
-
-
-              <InfiniteScroll
-                dataLength={pages.length}
-                next={fetchMoreData}
-                hasMore={hasMore}
-                loader={<h4>Loading...</h4>}
-                className={styles.mainSlideShow}
-                style={{margin:"0 auto"}}
-              >
-                {pages ? pages.map((article,index)=>{
-                  return(
-                    <div  key={index}>
-                    <Slide article={article} style={{height:"100%"}}/>
-                    {/* {(Math.floor(Math.random()* 10) > 8 )&& (
-                      <Ads300>
-                        <amp-ad 
-                          width="300" 
-                          height="200"
-                          type="adsense"
-                          data-ad-client="ca-pub-5455960452945884"
-                          data-ad-slot="5221773298"
-                          data-auto-format="rspv"
-                          data-full-width="">
-                            <amp-img
-                              src="/images/adPlaceholder300.png"
-                              width="300"
-                              height="250"
-                              layout="responsive"
-                              alt="placeholder"
-                          ></amp-img>
-                        </amp-ad>
-                      </Ads300>
-                    )} */}
-                    </div>
-                  )
-                }):(
-                  <></>
-                )}
-              </InfiniteScroll>
-            </div>
-              
+  return (
+    <div className={styles.editorial}>
+      <AmpStoryPlayer/>
+      <div className={styles.editorialContent}>
+        <div
+          className={styles.mainSlideShow}
+        >
+          {page1 ? page1.map((article,index)=>{
+            return(
+              <Slide key={index} article={article} style={{height:"100%"}}/>
+            )
+          }):(
+            <></>
+          )}
         </div>
-    )
+        
+        {/* <TopicSlider/> */}
+
+        <InfiniteScroll
+          dataLength={pages.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          className={styles.mainSlideShow}
+          style={{margin:"0 auto"}}
+        >
+          {pages ? pages.map((article,index)=>{
+            return(
+              <div  key={index}>
+              <Slide article={article} style={{height:"100%"}}/>
+              {/* {(Math.floor(Math.random()* 10) > 8 )&& (
+                <Ads300>
+                  <amp-ad 
+                    width="300" 
+                    height="200"
+                    type="adsense"
+                    data-ad-client="ca-pub-5455960452945884"
+                    data-ad-slot="5221773298"
+                    data-auto-format="rspv"
+                    data-full-width="">
+                      <amp-img
+                        src="/images/adPlaceholder300.png"
+                        width="300"
+                        height="250"
+                        layout="responsive"
+                        alt="placeholder"
+                    ></amp-img>
+                  </amp-ad>
+                </Ads300>
+              )} */}
+              </div>
+            )
+          }):(
+            <></>
+          )}
+        </InfiniteScroll>
+
+      </div>        
+    </div>
+  )
 }
