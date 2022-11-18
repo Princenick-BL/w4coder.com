c0e895fdbabd4936770812293a2478e1d71c33fa
PrinceNick-BL <princenickballo@gmail.com>
2022-11-18 23:44:53 +0100

    wip

---
+++ components/Ads300/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default function Ads300x600() {
+  return (
+    <div className={styles.ads300x300}>300x200</div>
+  )
+}
