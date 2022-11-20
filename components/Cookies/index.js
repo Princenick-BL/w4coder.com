344f91681cb40f6c0fb8528e3497c1f872c43091
PrinceNick-BL <princenickballo@gmail.com>
2022-11-20 14:53:55 +0100

    wip

---
+++ components/Cookies/index.js
@@ -0,0 +1,52 @@
+import React, { useState ,useEffect} from 'react'
+import styles from './index.module.scss'
+
+export default function Cookies() {
+
+    const [cookies,setCookies] = useState({
+        accept : false,
+        all : true,
+    })
+
+    function setTheme(accept) {
+        sessionStorage.setItem('used-cookies', {
+            accept : accept,
+            all : true,
+        });
+        setCookies({
+            accept : accept,
+            all : true,
+        })
+    }
+    useEffect(() => {
+        const cookie = sessionStorage.getItem('used-cookies')
+        setCookies(cookie)
+    }, [])
+    
+    return (
+        <>
+        {!cookies && (
+        <div className={styles.cookies +  " cookies"}>
+            <div className={styles.info}>
+                <div className={styles.txt}>
+                    <span>
+                        Nous utilisons des cookies et des méthodes similaires pour reconnaître les visiteurs et mémoriser leurs préférences. Nous les utilisons également pour mesurer l'efficacité des campagnes publicitaires, cibler les annonces et analyser le trafic du site. Pour en savoir plus sur ces méthodes, y compris sur la manière de les désactiver, consultez notre politique en matière de cookies.                
+                    </span>
+                    
+                    <br></br>
+                    <span>
+                        En cliquant sur "accepter", vous consentez à l'utilisation de ces méthodes par nous et par des tiers. Vous pouvez toujours modifier vos préférences en matière de suivi en consultant notre politique en matière de cookies.                
+                    </span>
+
+                </div>
+                <div className={styles.buttons}>
+                    <button onClick={(e)=>{setTheme(true)}}>ACCEPT</button>
+                    <button onClick={(e)=>{setTheme(false)}}>REJECT</button>
+                </div>
+            </div>
+        </div>
+
+        )}
+        </>
+    )
+}
