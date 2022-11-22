4dcc6b877e08914969d080514e4bb43a46509f13
PrinceNick-BL <princenickballo@gmail.com>
2022-11-22 20:41:05 +0100

    wip

--- components/Ads300/index.js
+++ components/Ads300/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import styles from './index.module.scss'
 
-export default function Ads300x600() {
+export default function Ads300x600({children}) {
   return (
-    <div className={styles.ads300x300}>300x200</div>
+    <div className={styles.ads300x300}>
+      {/* <ins class="adsbygoogle" style={{ display: "block" }} data-ad-client="ca-pub-987************676" data-ad-slot="776****95" data-ad-format="auto" data-full-width-responsive="true"    > </ins>      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </script> */}
+      <div className={styles.adInfo}>ADVERTISEMENT</div>
+      <div className={styles.adContent}>
+        300x300
+      </div>
+    </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useState,useEffect,useCallback } from 'react';
 import '../styles/globals.scss'
+import Script from 'next/script';
 
 
 const useMediaQuery = (width) => {
@@ -66,6 +67,14 @@ function MyApp({ Component, pageProps }) {
   },[])
   return(
     <div id='app'  className={themeDark===false ? "theme-light" :themeDark===true ? "theme-dark" : ""}>
+      {/* <Script 
+        id="Adsense-id" 
+        async    
+        onError={(e) => { console.error("Script failed to load", e);}}   
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-987************676"    
+        crossorigin="anonymous"  
+      /> */}
       <Component  {...pageProps} isBreakpoint={isBreakpoint} toggleTheme={(e)=>toggleTheme(e)}/>
     </div>
   )
