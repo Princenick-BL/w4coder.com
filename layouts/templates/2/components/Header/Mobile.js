e952de5a79c4a23f9765769bb2a2c1ea0418bacb
PrinceNickBALLO <prince@zmooz.com>
2023-03-31 14:42:32 +0200

    wip

--- layouts/templates/2/components/Header/Mobile.js
+++ layouts/templates/2/components/Header/Mobile.js
@@ -48,7 +48,6 @@ export default function Mobile({style,border=true,stories}) {
       if(e?.target?.value?.length > 0){
         mainHeaderResult.classList.add('searching')
         const res = await searchArticle(e?.target?.value)
-        console.log(res)
         setSearchResult(res)
       }else{
         mainHeaderResult.classList.remove('searching')
@@ -90,9 +89,9 @@ export default function Mobile({style,border=true,stories}) {
               <div className={styles.fhW}>
                 <div className={styles.hW} onClick={(e)=>{setOpenedMenu(!openedMenu)}}>
                   {openedMenu ? (
-                    <svg  className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 1 20 20"><path fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"></path></svg>
+                    <svg  className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 2 20 20"><path fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"></path></svg>
                   ):(
-                    <svg className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 1 20 20"><path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path></svg>
+                    <svg className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 2 20 20"><path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path></svg>
                   )}
                 </div>
                 <h2>W4CODER</h2>
@@ -112,7 +111,7 @@ export default function Mobile({style,border=true,stories}) {
                       marginRight:"1rem"
                     }}
                     className='hamburger' 
-                    onClick={(e)=>{setOpenedMenu(!openedMenu)}}
+                    onClick={(e)=>{setOpenedSearch(!openedMenu)}}
                     focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
                   <select defaultValue={lang} defaultChecked={lang} style={{
                       background:"transparent",
