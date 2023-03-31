17a4f253e3b2f709730e04e4d30789b4abf13e41
PrinceNickBALLO <prince@zmooz.com>
2023-03-31 13:22:47 +0200

    wip

--- layouts/templates/2/components/Header/Mobile.js
+++ layouts/templates/2/components/Header/Mobile.js
@@ -90,12 +90,12 @@ export default function Mobile({style,border=true,stories}) {
               <div className={styles.fhW}>
                 <div className={styles.hW} onClick={(e)=>{setOpenedMenu(!openedMenu)}}>
                   {openedMenu ? (
-                    <svg  className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"></path></svg>
+                    <svg  className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"></path></svg>
                   ):(
-                    <svg className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path></svg>
+                    <svg className='hamburger' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path></svg>
                   )}
                 </div>
-                <strong>W4CODER</strong>
+                <h2>W4CODER</h2>
               </div>
               {/* <Logo style={{fontSize:"1.5rem"}}  w={40} h={40} /> */}
               <div
